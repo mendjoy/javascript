7054b49c515d5be17a423d268177350d27a9b690
Joyce M Miranda <joycemendes.jm79@gmail.com>
2022-07-08 02:12:49 -0300

    update

---
+++ js-udemy/aula010/tiposprimitivos.js
@@ -0,0 +1,18 @@
+//strings
+const nome='Joyce';
+const nome1='Mendes'; 
+const nome2='Miranda';
+
+//number
+const num1=10;
+const num2=10.52; //Número com ponto flutuante
+
+
+let nomeAluno; //Valor indefinido, não aponta para local nenhum na memória
+let sobrenomeAluno=null; //nulo, não aponta para local nenhum na memória
+
+//Boolean (Logico)
+const boolean=true; //true or false
+
+
+
