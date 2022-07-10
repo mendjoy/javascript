08bfd3d0087bd26796737ee92bc59350ab06d4d1
Joyce M Miranda <joycemendes.jm79@gmail.com>
2022-07-10 10:37:56 -0300

    update aulas

--- js-udemy/aula14/number.js
+++ js-udemy/aula14/number.js
@@ -1,9 +1,15 @@
-let num1 = 1500; //number
+//IEE 754-2008 Padrão que o js segue para os números
+
+let num1 = 1500.00; //number
 let num2 = 2.5; //number
 
 console.log(num1.toString); //continua sendo um number
 num1= num1.toString(num1); //converter o number para string
 console.log(num1.toString(2)); //mostra a reprsentação binaria da variavel
 console.log(num1.toFixed(2)); //Mostra as casas decimais de acordo o indicado
+console.log(Number.isInteger()); //Retorna true or false, se o número for inteiro
+
+let temp = num1 * 'olá';
+console.log(Number.isNaN); //Retorna se é uma operação invalida (true or false)
 
 
---
+++ js-udemy/aula15/math.js
@@ -0,0 +1,18 @@
+
+//Arredondar para baixo 
+let num1 = 9.54578;
+let num2 = Math.floor (num1);  
+console.log(num2);
+
+//Arredondar para cima 
+let num3 = 9.54578;
+let num4 = Math.ceil (num3); 
+console.log(num4);
+
+//Arredondar para o mais proximo ex 1.5 para 2, e 1.4 para 1
+let num5 = 9.54578;
+let num6 = Math.round (num5); 
+console.log(num6);
+
+
+
