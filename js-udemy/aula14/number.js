be8e1742a090bf37ce7d7458caa2d0f704abd602
Joyce M Miranda <joycemendes.jm79@gmail.com>
2022-07-10 00:38:15 -0300

    update

---
+++ js-udemy/aula14/number.js
@@ -0,0 +1,9 @@
+let num1 = 1500; //number
+let num2 = 2.5; //number
+
+console.log(num1.toString); //continua sendo um number
+num1= num1.toString(num1); //converter o number para string
+console.log(num1.toString(2)); //mostra a reprsentação binaria da variavel
+console.log(num1.toFixed(2)); //Mostra as casas decimais de acordo o indicado
+
+
