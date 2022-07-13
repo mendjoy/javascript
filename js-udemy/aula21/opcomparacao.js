9f26a6b6ee1976fab41646e0993077cec43cb39e
Joyce M Miranda <joycemendes.jm79@gmail.com>
2022-07-13 15:54:17 -0300

    operadores de comparação

---
+++ js-udemy/aula21/opcomparacao.js
@@ -0,0 +1,29 @@
+/* 
+*Operadores de comparação
+* > maior que 
+* >= maior que ou igual que
+* < menor que
+* <= menor que ou igual a 
+* == igualdade (checa valor)
+* === igualdade estrita (checa valor e tipo )
+* != diferente  (checa valor )
+* !== diferente estrito (checa valor e tipo)
+*/
+
+console.log(10 > 5); // true - Retorna True ou False
+console.log(10 < 5); //false 
+
+console.log(10 >=5); //Checagem se é maior ou igual, se uma delas for true, a expressão é true 
+console.log(5 <=5); //Checagem se é menor ou igual, se uma delas for true, retornara true 
+
+console.log(5 == '5'); //true, checa o valor e não o tipo, retorna true ou false
+console.log(5 === '5'); // false, checa valor e tipo
+console.log(5 === 5); //true 
+
+console.log( 5 != '6'); // true, retorna true ou false, checa valor e não tipo 
+console.log(5 !== 6); // true 
+console.log(5 !== '6'); //false
+
+
+
+
