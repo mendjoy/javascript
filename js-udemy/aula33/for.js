ccc05895a6dea24369b74fc9959be0b3710852ba
Joyce M Miranda <joycemendes.jm79@gmail.com>
2022-07-19 10:40:11 -0300

    Estrutura de repetição (for)

---
+++ js-udemy/aula33/for.js
@@ -0,0 +1,8 @@
+//Estruturas de repetição (for)
+
+
+// dentro do (), primeiro criar variavel e indicar o inicio, segundo condição (onde parar), terceiro  incrementação (a cada volta do laço)
+for (let i = 0; i <= 5; i ++ ) {
+    console.log(`Linha ${i}`);
+} //Nao usar ; no final
+
