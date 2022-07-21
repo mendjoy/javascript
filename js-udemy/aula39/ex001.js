2d7f5013d2ba8199012f9ac31824688e83f5de53
Joyce M Miranda <joycemendes.jm79@gmail.com>
2022-07-21 15:21:28 -0300

    Exercicios (Lógica da programação)

---
+++ js-udemy/aula39/ex001.js
@@ -0,0 +1,12 @@
+//Escreva uma função que recebe 2 números e retorne o maior deles 
+
+function max(x, y) {
+    if (x > y){
+        return x
+    } else {
+        return y 
+    }
+}; 
+const maior = max(1, 2);
+console.log(maior);
+
