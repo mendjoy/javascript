59a89393172e40ae1a4ea319ccb90fd3b8057d78
Joyce M Miranda <joycemendes.jm79@gmail.com>
2022-07-23 23:53:27 -0300

    Tratando e lançando erros (try, catch, throw)

--- js-udemy/aula40/index.js
+++ js-udemy/aula40/index.js
@@ -24,3 +24,11 @@ try {
     }
 
 
+    try {
+        //é executada quando não há erros 
+    } catch (e) {
+        //executada quando há erros 
+    } finally {
+        //sempre é executado (não é sempre utilizado, pode ser omitido)
+    }
+
--- js-udemy/aula40/index02.js
+++ js-udemy/aula40/index02.js
@@ -1,7 +1,27 @@
-try {
-    //é executada quando não há erros 
-} catch (e) {
-    //executada quando há erros 
-} finally {
-    //sempre é executado (não é sempre utilizado, pode ser omitido)
+function retornaHora (data) {
+    if ( data && !(data instanceof Date)){
+        console.log("Não é")
+        throw new TypeError ('Esperando instcancia de Date.')
+    };
+
+if (! data){
+    data = new Date();
 }
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        minute: '2-digit',
+        second:'2-digit',
+        hour12: false
+    }); 
+
+};
+
+try {
+    const data = '01-01-1970 12:58:12'; 
+    const hora = retornaHora(11); 
+    console.log(hora);
+} catch(e){
+    //tratar erro
+} finally {
+    console.log('Tenha um bom dia')
+}; 
